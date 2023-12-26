6a9df7277487035362bc4ce58c7aa265d259b980
Aditya-Raj-009 <adityasinghrajput2107200@gmail.com>
2023-12-26 23:12:01 +0530

    updated code

--- src/main/kotlin/_18_Inheritance.kt
+++ src/main/kotlin/_18_Inheritance.kt
@@ -1,7 +1,7 @@
 fun main()
 {
-val basicPhone:BasicPhone = BasicPhone();
-    val smrt:SmartPhone = SmartPhone();
+val basicPhone:BasicPhone = BasicPhone("Black");
+    val smrt:SmartPhone = SmartPhone("Red");
     println(basicPhone.name)
     println(basicPhone.volume)
     println(smrt.getDeviceInfo())
@@ -13,13 +13,21 @@ But Kotlin has a special features i.e. classes and methods are not open for exte
 are by default public and final class or final function. It means Open classes and methods in kotlin are equivalent to the opposite
 of final in java, an open method is overridable and open class is extendable in kotlin.
  */
-open class Phone{
+open class Phone {
     init {
         println("I am Parent (Phone class)")
     }
-   open val name:String = "Basic"
-   val type:String = "Basic 1100";
-    val volume:Int = 10;
+
+    open val name: String = "Basic"
+    val color:String
+    val type: String = "Basic 1100";
+    val volume: Int = 10;
+
+    constructor(color: String)
+    {
+        this.color = color;
+    }
+
 
     fun makeCall(){}
     fun display(){}
@@ -32,18 +40,26 @@ open class Phone{
 
 // Inheritance: As like Java, Kotlin doesn't support multiple Inheritance.
 
-class BasicPhone: Phone()
+class BasicPhone:Phone
 {
     init {
         println("I am child Basic phone")
     }
      override val name:String = "Nokia"
+    constructor(color:String):super(color)
+    {
+
+    }
  fun getScreenInfo(){}
 }
 
-class SmartPhone: Phone(){
+class SmartPhone: Phone{
     init {
         println("I am child Smart phone")
+    }
+    constructor(color:String):super(color)
+    {
+
     }
     override fun getDeviceInfo():String {
        return  "I am smartPhone";
