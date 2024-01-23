6cbd210b2879cf1d22fa31ae11078ab7a1fa17fd
Aditya-Raj-009 <adityasinghrajput2107200@gmail.com>
2024-01-24 01:22:13 +0530

    Updated code

--- src/main/Practice/prac3.kt
+++ src/main/Practice/prac3.kt
@@ -1,110 +1,246 @@
-package main.Practice
-
-class prac3  constructor(name:String,age:Int,id:String,email_id:String)
-{
-    private val u_name = name
-    private val age:Int = age;
-    private val email_id : String = email_id
-    private val id:String = id;
-    fun name():Unit
-    {
-       println("Username is $u_name");
-    }
-    fun age():Unit = println("User age is $age")
-    fun email():Unit = println("User email is $email_id")
-    fun id():Unit = println("User id is $id")
-
-    fun canVote():String = if(age>18) "Yes" else "No"
-
-}
-
-open class Animal (type:String = "Animal", color:String="Black",leg:Int=4,hand:Int=0,classType:String = "Animal")
-{
-   protected val type:String = type
-    protected val cType: String = classType
-    protected  val color:String = color
-    protected val leg:Int = leg
-    protected val hand:Int = hand
-
-
-    init {
-        println("I am $classType class of type $type, color $color with leg $leg and hand $hand")
-
-    }
-
-
-    constructor(type:String="Human",classType: String):this(type,"white",2,2,classType)
-    {
-
-        println("Nice to meet you! You are a $type , color $color with leg $leg and hand $hand\" ")
-    }
-
-   open fun type():Unit = println("Class Type: $cType\nAnimal type: $type\nColor: $color\nLeg: $leg\nHand: $hand")
-
-    open fun className():Unit = println("Class Type: $cType")
-
-    open fun greet():Unit = println("Hi! Animal")
-
-    fun checkAnim() = println("Animal")
-}
-
-class Human(type: String,classType: String) : Animal(type,classType)
-{
-    override fun type():Unit = println("Class Type: $cType\nAnimal type: $type\nColor: $color\nLeg: $leg\nHand: $hand")
-     override fun className():Unit = println("Class Type: $cType")
-
-
-     override fun greet():Unit = println("Hi! Human")
-    fun checkHum() = println("Human")
-}
+import main.kotlin.*
 
 fun main() {
-//    val prac:prac3 = prac3(name = "Aditya Raj",18,"adit12333","adityaraj@gmail.com")
+//    var a:Int = 28
+//    var b:Int = 49
+//    var c:Int = 38
 //
-//    prac.name()
-//    prac.age()
-//    prac.email()
-//    prac.id()
+//    println("Greatest number among $a , $b and $c is ${if(a>b && a>c){
+//        a
 //
-//    println("Am I eligible to vote: ${prac.canVote()}")
+//    } else if (b>a && b>c){
+//        b
+//    }
+//        else{
+//            c
+//    }
+//    }")
 //
-//    val animal:Animal = Animal()
-//    animal.type()
+//    println("${when(a){
+//        49-> "Hello I am number 49"
+//        38-> "Hello I am number 38"
+//        else-> "Hello I am number 28"
 //
-//    val human:Animal = Human("Human","Human")
-//    human.className()
-//    human.greet()
-//    human.checkAnim()
+//    }    }")
+
+//
+//    var arr:Array<Int> = arrayOf<Int>(9,4,62,4,6,3,5)
+//    var arr2:Array<String> = arrayOf("Hey","Hello","How", "Are","You","?")
+//
+//    for (i in 0 until arr.size)
+//    {
+//        println(arr[i])
+//    }
+//    for ((i:Int,j:String) in arr2.withIndex())
+//    {
+//        println("$i th element is $j")
+//    }
+//  for(i in arr.size-1 downTo 0)
+//  {
+//      print(arr.get(i))
+//  }
+//    println(arr.contentToString())
+
+//    println(add(5,7,8,9))
+//    println(substract(13,4))
+//    println(substract(13,4,c=9))
+//        var smrt:Phone2 = SmartPhone2("SmartPhone","5G","WIFI_Bluetooth")
+//
+//    smrt.wifiType = "Wifi6"
+//    smrt.MobileType()
+//    smrt.NetworkType()
+//    smrt.WifiType()
 
 
-    var imgUri:String = "content://com.miui.gallery.open/raw/%2Fstorage%2Femulated%2F0%2FAndroid%2Fmedia%2Fcom.whatsapp%2FWhatsApp%2FMedia%2FWhatsApp%20Images%2FIMG-20231229-WA0007.jpg"
-    var img2Uri:String = "content://com.miui.gallery.open/raw/%2Fstorage%2Femulated%2F0%2FAndroid%2Fmedia%2Fcom.whatsapp%2FWhatsApp%2FMedia%2FWhatsApp%20Images%2FIMG-20231230-WA0014.jpg"
+//    var ad:(Int,Int)->Int = ::add
+//    println(ad(5,6))
+//
+//    var car:Maruti = Maruti("Car",4)
+//
+//    car.vehicleName("Maruti")
+//
+//    var truck:Vehicle = Maruti("Truck",12)
+//    truck.vehicleName("Truck")
 
-    println(matchPercent(img2Uri,imgUri))
+//    var obj:clone = Object()
+//    obj.cloneObject("Repo")
+//
+//    var obj2:drag = Object()
+//    obj2.dragObject("Player")
+//    var char:Char ='A'
+//    var ascci:Int = char as Int
+//    println(ascci)
 
+    var arr:Array<Shape> = arrayOf(Circle(6.5),Triangle(5.6,2.9),Square(5.6))
 
-}
-
-private fun matchPercent(img1:String,img2:String):Double
-{
-    var img1Size:Int = img1.length
-    var img2Size:Int = img2.length
-    val maxImgSize: Int = Math.max(img1Size,img2Size)
-    var matchPercentage:Double
-    var match:Double=0.0
-    var j:Int = 0
-    for(i in img1)
+    for (i:Shape in arr)
     {
-        if(i == img2[j++])
+        if(i is Circle)
         {
-            match++
+            println(i.area())
+        }
+        else if (i is Square)
+        {
+            i.squareClass()
+        }
+        else{
+            (i as Triangle).triangleClass()
         }
     }
-    matchPercentage = ((match/maxImgSize)*100).toDouble()
-    return matchPercentage
-}
-private fun String.isEqualImageUri(imgUri:String):Boolean
-{
 
-    return matchPercent(this,imgUri)>=90
+    if(arr[0] !is Triangle)
+    {
+        println("I am not a triangle")
+    }
 }
+
+
+
+
+
+fun add(vararg a:Int):Int
+{
+    var sum = 0
+    for(i in 0 until a.size)
+    {
+        sum+=a.get(i)
+    }
+    return sum
+}
+//fun substract(a:Int,b:Int,c:Int = 1):Int = a-b+c;
+//
+
+
+
+
+//open class Phone2(var phnType:String ,   var networkType:String,wirelessType:String)
+//{
+//
+//    var WLType:String
+//    open var wifiType: String? = null
+//
+//    init {
+//
+//        WLType = wirelessType
+//       println("This is Phone2 class")
+//    }
+//
+//    constructor(phnType: String):this(phnType,"2G","BluetoothOnly")
+//    {
+//        println("This is secondary constructor of Phone2")
+//    }
+//    open fun wirelessType():Unit
+//    {
+//        println("Wireless type of this phone is $WLType")
+//
+//    }
+//   open fun NetworkType()
+//    {
+//        println("Network Type of mobile is $networkType")
+//    }
+//    open fun MobileType():Unit = println("Mobile type is $phnType")
+//
+//   open fun WifiType():Unit = println("Wifi type is ${wifiType}")
+//
+//
+//
+//}
+//
+//class SmartPhone2(phnType:String,  networkType:String, wirelessType:String)
+//    :Phone2(phnType,networkType,wirelessType)
+//{
+//        init {
+//            println("This is Smartphone class")
+//        }
+//
+//
+//    override var wifiType: String? = null
+//        get() = field
+//        set(value) {
+//            field = value
+//        }
+//   override fun wirelessType():Unit
+//    {
+//        println("Wireless type of this smartphone is $WLType")
+//
+//    }
+//
+//    override fun NetworkType() {
+//        println("Network type of this smart phone is $networkType")
+//    }
+//
+//  override  fun MobileType():Unit = println("SmartPhone type is $phnType")
+//
+//    override fun WifiType():Unit = println("Wifi type is ${wifiType}")
+//
+//
+//}
+
+//abstract class Vehicle(var vehicleType:String,var noOfWheel:Int)
+//{
+//    init {
+//        println("I am abstract class.")
+//
+//    }
+//
+//    abstract fun vehicleName(vehicleName:String):Unit
+//
+//
+//
+//
+//}
+//
+//class Maruti(vehicleType: String,noOfWheel: Int):Vehicle(vehicleType,noOfWheel)
+//{
+//    override fun vehicleName(vehicleName:String) {
+//        println("$vehicleName $noOfWheel")
+//    }
+//}
+
+//interface drag
+//{
+//var dragSpeed:Int
+//fun AvgSpeed()
+//{
+//    println("Average Drag speed is $dragSpeed m/s")
+//}
+//    fun dragObject(name:String)
+//
+//
+//}
+//
+//interface clone
+//{
+//    var cloneSpeed:Int
+//
+//    fun cloneObject(name:String)
+//
+//    fun AvgSpeed()
+//    {
+//        println("Average cloning speed is $cloneSpeed m/s")
+//    }
+//}
+//
+//class Object:clone,drag
+//{
+//    override var cloneSpeed: Int = 6
+//    override var dragSpeed: Int = 8
+//    override fun AvgSpeed() {
+//       super<clone>.AvgSpeed()
+//    }
+//
+//
+//    override fun cloneObject(name: String) {
+//        println("$name is cloning..")
+//        super<clone>.AvgSpeed()
+//
+//    }
+//
+//
+//    override fun dragObject(name:String) {
+//        println("$name is dragging..")
+//        super<drag>.AvgSpeed()
+//    }
+//
+//
+//}
--- src/main/kotlin/_24_TypeChecking and smart casting.kt
+++ src/main/kotlin/_24_TypeChecking and smart casting.kt
@@ -37,5 +37,47 @@ this is smart casting ,means it automatically suggest the method of that class o
 
     }
 
+    type(array[0])
+
+
+//    "Unsafe" cast operator":
+    /*
+    Usually, the cast operator throws an exception if the cast isn't possible.
+    Thus, it's called unsafe. The unsafe cast in Kotlin is done by the infix operator as.
+
+    Note that null cannot be cast to String, as this type is not nullable.
+    If y is null, the code below throws an exception:
+     */
+
+    var y:Int? = null
+//    var x:String = y as String  // throw an exception
+
+    /*
+    To make code like this correct for null values,
+    use the nullable type on the right-hand side of the cast:
+     */
+    val x: String? = y as String?   // now it will not throw exception
+    println(x)
+
+
+
+//    "Safe" (nullable) cast operator":
+//    To avoid exceptions, use the safe cast operator as?, which returns null on failure:
+
+    val x2: String? = y as? String
+    println(x2)
+
+
+
+}
+
+fun type(s:Shape)
+{
+    if(s !is Triangle) // Same as: !(s is String)
+    {
+        println("Given shape is not a triangle")
+        return
+    }
+    println("Area of triangle is ${s.area()}")
 }
 
